a53a9a29b867689f05d9921a773a1cf28c51b8ef
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-06-05 23:48:24 +0530

    Fix `Confidence` formula

--- Apriori/Algorithm/Calculate.cs
+++ Apriori/Algorithm/Calculate.cs
@@ -4,6 +4,6 @@ internal static class Calculate
   // A => Subset, B => Subset
   // AB => A implies B
   internal static double Support(in int frequencyAB, in int transactions) => (double)frequencyAB / transactions;
-  internal static double Confidence(in int frequencyAB, in int frequencyA) => (double)frequencyAB / frequencyA;
+  internal static double Confidence(in double supportAB, in double supportA) => supportAB / supportA;
   internal static double Lift(in double confidenceAB, in double supportB) => confidenceAB / supportB;
 }
