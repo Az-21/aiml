4dd6c83f62db9ee66c2581bb3ea739d4051c0204
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-06-05 22:49:19 +0530

    Call indicator functions against subsets

--- Apriori/Program.cs
+++ Apriori/Program.cs
@@ -18,5 +18,34 @@ internal static class Program
 
     // Get frequency of all subsets from the original CSV
     Frequency.UpdateFrequencyInplace(ref subsets, in config);
+
+    // Get total number of transactions
+    int transactions = Frequency.CountNonEmptyLines(in config);
+
+    // Calculate support, confidence, and list
+    Console.WriteLine();
+    foreach (KeyValuePair<HashSet<string>, int> subset in subsets)
+    {
+      // Ignore sets with one item (basket analysis does not apply)
+      if (subset.Key.Count <= 1) { continue; } // <=1 to account for null subset
+
+      // Split subset into {A} || {B} = {x1, x2, ..., xN-1} || {xN}
+      HashSet<string> subsetAB = subset.Key;
+      HashSet<string> subsetA = new(subsetAB.Take(subsetAB.Count - 1));
+
+      // Print subset rule in A => B format
+      string a = "{ " + string.Join(", ", subsetA) + " }";
+      string b = subsetAB.Last();
+      Console.WriteLine($"{a} => {b}");
+
+      // Calculate analysis indicators
+      double support = Calculate.Support(subsetAB.Count, in transactions);
+      double confidence = Calculate.Confidence(subsetAB.Count, subsetA.Count);
+      double lift = Calculate.Lift(in confidence, in support);
+
+      Console.WriteLine($"Support = {support}");
+      Console.WriteLine($"Confidence = {confidence}");
+      Console.WriteLine($"Lift = {lift}\n");
+    }
   }
 }
