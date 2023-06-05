50c96c7a045a0748410c1bef49fea8c3f1fc623c
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-06-05 23:14:56 +0530

    Fix `lift` formula

--- Apriori/Algorithm/Calculate.cs
+++ Apriori/Algorithm/Calculate.cs
@@ -5,5 +5,5 @@ internal static class Calculate
   // AB => A implies B
   internal static double Support(in int frequencyAB, in int transactions) => (double)frequencyAB / transactions;
   internal static double Confidence(in int frequencyAB, in int frequencyA) => (double)frequencyAB / frequencyA;
-  internal static double Lift(in double confidence, in double support) => confidence / support;
+  internal static double Lift(in double confidenceAB, in double supportB) => confidenceAB / supportB;
 }
