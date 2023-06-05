8525eca3bdf66962ec36595c3b53117d3d3d6334
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-06-05 21:53:36 +0530

    Add function to create subsets
    A, B, C
    
    A, B, C, AB, AC, BC, ABC

---
+++ Apriori/Algorithm/Subset.cs
@@ -0,0 +1,27 @@
+﻿namespace Apriori.Algorithm;
+internal static class Subset
+{
+  internal static Dictionary<HashSet<string>, int> Generate(in Dictionary<string, int> input)
+  {
+    Dictionary<HashSet<string>, int> subsets = new(HashSet<string>.CreateSetComparer())
+    {
+      { new HashSet<string>(), 0 }
+    };
+
+    // Generate subsets
+    foreach (var item in input)
+    {
+      int count = item.Value;
+      foreach (HashSet<string> subset in (HashSet<HashSet<string>>)new(subsets.Keys, HashSet<string>.CreateSetComparer()))
+      {
+        for (int i = 1; i <= count; i++)
+        {
+          HashSet<string> newSubset = new(subset) { item.Key };
+          if (!subsets.ContainsKey(newSubset)) { subsets.Add(newSubset, 0); }
+        }
+      }
+    }
+
+    return subsets;
+  }
+}
--- Apriori/Program.cs
+++ Apriori/Program.cs
@@ -13,5 +13,7 @@ internal static class Program
     // Get frequency of individual items => Prune based on minimum frequency
     Dictionary<string, int> items = Frequency.OfIndividualItemsWithMinimumThreshold(in config);
 
+    // Generate all possible subsets from the pruned items dictionary
+    Dictionary<HashSet<string>, int> subsets = Subset.Generate(items);
   }
 }
